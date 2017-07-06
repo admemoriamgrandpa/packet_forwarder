42f32caa4edcbb8a5ce15cf99d63b06945175bd9
admemoriamgrandpa <nwabuonastanley@gmail.com>
2017-07-06 18:35:03 +0200

    Update poly_pkt_fwd.c

--- poly_pkt_fwd/src/poly_pkt_fwd.c
+++ poly_pkt_fwd/src/poly_pkt_fwd.c
@@ -1659,7 +1659,7 @@ void thread_up(void) {
 							MSG("ERROR: [up] snprintf failed line %u\n", (__LINE__ - 4));
 							exit(EXIT_FAILURE);
 						}
-						j = snprintf((char *)(buff_up + buff_index), TX_BUFF_SIZE-buff_index, ",\"tmms\":%llu", (x->tm_hour)*3.6E12 + (x->tm_min)*6E10 + (x->tm_sec)*1E9 + (pkt_utc_time.tv_nsec)); /* ISO 8601 format */
+						j = snprintf((char *)(buff_up + buff_index), TX_BUFF_SIZE-buff_index, ",\"tmms\":%llu", (x->tm_hour)*36*1E11 + (x->tm_min)*6E10 + (x->tm_sec)*1E9 + (pkt_utc_time.tv_nsec)); /* ISO 8601 format */
 						if (j > 0) {
 							buff_index += j;
 						} else {
